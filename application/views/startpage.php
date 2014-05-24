a952600df18400f6a389f9b2ca2d1190a3cf9b14
danielronnkvist <daniel@ronnkvist.com>
2014-05-24 10:53:26 +0200

    Started working on a shell

--- application/views/startpage.php
+++ application/views/startpage.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
@@ -5,8 +6,28 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Capture</title>
 
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/bootstrap.min.js">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/style.css">
+
+
 </head>
 <body>
+	<header id="header">
+		<h1>SUMMER TIME</h1>
+	</header>
+	<div id="main">
 
+	</div>
+	<footer id="footer">
+		<div class="container">
+			<p>Bajsas ut av Orups Vargar</p>
+		</div>
+	</footer>
+
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
 </body>
 </html>
