457e88deb29143e642540c7bd7770b13b358dd46
danielronnkvist <daniel@ronnkvist.com>
2014-05-24 12:59:26 +0200

    Spotify added, footer with icons N shit

---
+++ application/views/templates/footer.php
@@ -0,0 +1,40 @@
+			</div>
+		</div>
+		<div class="push"></div>
+	</div>
+	<footer id="footer">
+		<div class="container">
+			<a href="#">
+				<div class="col-xs-4">
+					<span class="glyphicon glyphicon-font col-xs-12 text-center"></span>
+					<span class="col-xs-12 text-center">
+						<h4>text</h4>
+					</span>
+				</div>
+			</a>
+			<a href="<?php echo base_url(); ?>">
+				<div class="col-xs-4">
+					<span class="glyphicon glyphicon-camera col-xs-12 text-center"></span>
+					<span class="col-xs-12 text-center">
+						<h4>camera</h4>
+					</span>
+				</div>
+			</a>
+			<a href="<?php echo site_url('pages/spotify'); ?>">
+				<div class="col-xs-4">
+					<center>
+						<img class="spotify-logo" src="<?php echo base_url(); ?>assets/img/spotify-logo-primary-vertical-light-background-rgb.jpg">
+					</center>
+				</div>
+			</a>
+		</div>
+	</footer>
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+    <!-- Spotify search -->
+	<script src="<?php echo base_url(); ?>/assets/js/spotify_search.js"></script>
+</body>
+</html>
---
+++ application/views/templates/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Capture</title>
+
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/style.css">
+
+
+</head>
+<body>
+	<div class="wrapper">
+		<header id="header">
+			<div class="container">
+				<h1 class="text-center">CAPTURE</h1>
+			</div>
+		</header>
+		<div id="main">
+			<div class="container">
